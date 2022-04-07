92780a33a384b7f97e4195c701979d28ca65118a
edrio <edriolich@gmail.com>
2022-04-07 16:39:13 +0700

    Testing Commit

---
+++ app/src/main/java/com/skripsi_edcanbil/qrin/MainActivity.java
@@ -0,0 +1,14 @@
+package com.skripsi_edcanbil.qrin;
+// Test Commit
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
